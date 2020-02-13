b76f6c77eeaf67254ad94cf0252ebc9387cbaaa7
PISANKINA <55401315+PISANKINA@users.noreply.github.com>
2020-02-13 14:12:58 +0700

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,25 @@
+import turtle
+
+
+def tree(hight, engle):
+    if engle == 0:
+        return engle
+    turtle.forward(hight)
+    turtle.right(30)
+    tree(hight / 2, engle - 1)
+    turtle.left(60)
+    tree(hight / 2, engle - 1)
+    turtle.right(30)
+    turtle.backward(hight)
+
+
+def main():
+    print("Добро пожаловать, вы рисуете дерево! Введите следующие параметры:")
+    hight = int(input('Пожалуйста, укажите высоту дерева: '))
+    engle = int(input('Пожалуйста, укажите величину угла: '))
+    turtle.left(90)
+    tree(hight, engle)
+
+
+if __name__ == '__main__':
+    main()
