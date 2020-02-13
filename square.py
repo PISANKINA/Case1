800df2242d8622d3620696aad647174dbd15b487
PISANKINA <55401315+PISANKINA@users.noreply.github.com>
2020-02-13 14:12:20 +0700

    Create square.py

---
+++ square.py
@@ -0,0 +1,25 @@
+import turtle
+
+
+def squere(n, a):
+    if n == 0:
+        return n
+    turtle.pendown()
+    for i in range(4):
+        turtle.forward(a)
+        turtle.right(90)
+    turtle.penup()
+    turtle.right(10)
+    turtle.forward(0.1 * a)
+    squere(n - 1, 0.9 * a)
+
+
+def main():
+    print("Добро пожаловать, вы рисуете квадрат! Введите следующие параметры:")
+    n = int(input("Глубина рекурсии: "))
+    a = int(input("Длину стороны квадрата: "))
+    squere(n, a)
+
+
+if __name__ == '__main__':
+    main()
