11b3af9067d8b80dd7e3c894e5bd1d060805645d
PISANKINA <55401315+PISANKINA@users.noreply.github.com>
2020-02-27 20:33:07 +0700

    Update snow_koch.py

--- snow_koch.py
+++ snow_koch.py
@@ -6,10 +6,13 @@ def snow_koch(n, size):
         turtle.forward(size)
     else:
         snow_koch(n - 1, size / 3)
+        turtle.color("red")
         turtle.left(60)
         snow_koch(n - 1, size / 3)
+        turtle.color("blue")
         turtle.right(120)
         snow_koch(n - 1, size / 3)
+        turtle.color("purple")
         turtle.left(60)
         snow_koch(n - 1, size / 3)
 
@@ -18,6 +21,7 @@ def main():
     print("Добро пожаловать, вы рисуете cнежинку Коха! Введите следующие параметры:")
     n = int(input("Глубина рекурсии: "))
     size = int(input("Длину стороны cнежинки Кохи: "))
+    turtle.bgcolor('pink')
     for i in range(3):
         snow_koch(n, size)
         turtle.right(120)
