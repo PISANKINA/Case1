ec31f492a1c0eeeb06e522e4f40669d2489f620a
PISANKINA <55401315+PISANKINA@users.noreply.github.com>
2020-02-27 20:31:06 +0700

    Create dragon.py

---
+++ dragon.py
@@ -0,0 +1,24 @@
+import turtle
+
+
+def dragon(c, height, n):
+    if n == 0:
+        return turtle.forward(height)
+    turtle.left(45 * c)
+    dragon(1, height, n - 1)
+    turtle.right(90 * c)
+    dragon(- 1, height, n - 1)
+    turtle.left(45 * c)
+
+
+def main():
+    height = int(input("Длину стороны ветки: "))
+    n = int(input("Глубина рекурсии: "))
+    turtle.color('pink')
+    c = 1
+    dragon(c, height, n)
+    turtle.mainloop()
+
+
+if __name__ == '__main__':
+    main()
